dd73679e098dbab3f26c4a2cba8a04ecbc74d37e
aliyazdi75 <ma.yazdi75@gmail.com>
2021-01-06 13:18:10 +0330

    Built build
    Branch: main
    Commit: 683fea02b906c34ccc228fe403afd98f8458bcf8 (dirty)
    
    package:peanut 3.5.1

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,35 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var C,H,J,P,W,U,Y,N={eL:function eL(){}},R,B,A,G,V,X,S,Z,E,T,K,L,O,D,F,Q,M
+a.setFunctionNamesIfNecessary([N])
+C=c[0]
+H=c[1]
+J=c[2]
+P=c[3]
+W=c[4]
+U=c[5]
+Y=c[6]
+N=a.updateHolder(c[7],N)
+R=c[8]
+B=c[9]
+A=c[10]
+G=c[11]
+V=c[12]
+X=c[13]
+S=c[14]
+Z=c[15]
+E=c[16]
+T=c[17]
+K=c[18]
+L=c[19]
+O=c[20]
+D=c[21]
+F=c[22]
+Q=c[23]
+M=c[24]
+N.eL.prototype={}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(N.eL,P.x)})()
+H.a5P(b.typeUniverse,JSON.parse('{"a8f":"P","a8V":"P","a8l":"f7","a8g":"q","a9m":"q","a9z":"q","a8e":"a1","a90":"a1","aa9":"df","a8o":"a_","a9A":"V","a8O":"V","a91":"eB","a9T":"dg","a8C":"eU","a8L":"e_","a8w":"dk","a9J":"dk","a9i":"hd","a93":"h6","a92":"h5","a8F":"bn","a8m":"hb"}'))
+0
+0}
+$__dart_deferred_initializers__["8351Z4lJAQfpxQvDH+TiLPKl+oA="] = $__dart_deferred_initializers__.current
---
+++ main.dart.js_2.part.js
@@ -0,0 +1,80 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var C,H,J,P,W,U,Y,N,R,B,A,G,V,X,S={
+alq:function(){X.po("en")
+return new S.l5()},
+l5:function l5(){}},Z,E,T,K,L,O,D,F,Q,M
+a.setFunctionNamesIfNecessary([S])
+C=c[0]
+H=c[1]
+J=c[2]
+P=c[3]
+W=c[4]
+U=c[5]
+Y=c[6]
+N=c[7]
+R=c[8]
+B=c[9]
+A=c[10]
+G=c[11]
+V=c[12]
+X=c[13]
+S=a.updateHolder(c[14],S)
+Z=c[15]
+E=c[16]
+T=c[17]
+K=c[18]
+L=c[19]
+O=c[20]
+D=c[21]
+F=c[22]
+Q=c[23]
+M=c[24]
+S.l5.prototype={
+gxv:function(){return"Ali Yazdi Homepage"},
+gHw:function(){return"ALI YAZDI"},
+gHx:function(){return"CE GRADUATED @ TEHRAN POLYTECHNIC"},
+gIS:function(){return"Hi there, welcome to my page"},
+EA:function(d){return"I am a Software Engineering graduate from Amirkabir University of Technology, which is one of the most prestigious universities in Iran, "+d+". And a Developer, who loves programming in Flutter. I am also interested in Artificial Intelligence, Machine Learning and Software Engineering."},
+gEC:function(){return"ranked second in IT"},
+gIg:function(d){return"Resume"},
+gEB:function(){return"ABOUT"},
+gG4:function(){return"EDUCATION"},
+gGg:function(){return"EXPERIENCE"},
+gF0:function(){return"AWARDS"},
+gET:function(){return"Amirkabir University of Technology"},
+gHM:function(){return"(Tehran Polytechnic)"},
+gEP:function(){return", Tehran, Iran"},
+gER:function(){return"B.Sc., Computer Engineering, Software"},
+gES:function(){return"September 2015"},
+gEQ:function(){return"January 2020"},
+gz5:function(){return"Adab High School"},
+gz1:function(){return"Tehran, Iran"},
+gz4:function(){return"September 2011"},
+gz2:function(){return"June 2014"},
+gz3:function(){return"Diploma in Physics and Mathematics"},
+gIv:function(){return"Tarafdari"},
+gIp:function(){return"Tehran, Iran"},
+gIx:function(){return"June 2019"},
+gIw:function(){return"April 2020"},
+gIu:function(){return"Flutter, Drupal, Laravel, MySQL"},
+gIq:function(){return"Tarafdari is the first and most popular sports social media network in Iran, providing official news and live scores of national and international matches."},
+gIr:function(){return"Studying and Researching about Flutter framework"},
+gIs:function(){return"Developing requirement APIs"},
+gIt:function(){return"Developing Tarafdari's client application and requirement plugins in Flutter, that is including multimedia content, live score matches, sports TV streaming with live chat on the socket"},
+gEV:function(){return"Application Page"},
+gHB:function(){return"July 2013"},
+gHC:function(){return"Qualified for the Second Round of the Iranian Physics Olympiad and Mathematical Olympiad"},
+gGd:function(){return"July 2015"},
+gGe:function(){return"Ranked in top 0.2% among more than 180K participants in Nation-Wide University Entrance Exam among all Iranian Student of Physics and Mathematics"},
+gF6:function(){return"February 2020"},
+F7:function(d){return"First Place in the 12th Iran Web and Mobile Festival("+d+") for the Best Sport Mobile Application among 7k applications"},
+gF8:function(){return"IWMF"},
+gHm:function(){return"Made with"},
+Ff:function(d){return"by "+d},
+gGq:function(){return"Flutter"}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(S.l5,N.eL)})()
+H.a5P(b.typeUniverse,JSON.parse('{"a8f":"P","a8V":"P","a8l":"f7","a8g":"q","a9m":"q","a9z":"q","a8e":"a1","a90":"a1","aa9":"df","a8o":"a_","a9A":"V","a8O":"V","a91":"eB","a9T":"dg","a8C":"eU","a8L":"e_","a8w":"dk","a9J":"dk","a9i":"hd","a93":"h6","a92":"h5","a8F":"bn","a8m":"hb","l5":{"eL":[]}}'))
+0
+0}
+$__dart_deferred_initializers__["PfcUYtxdMmSTeyJb7DgYbFLeM8k="] = $__dart_deferred_initializers__.current
