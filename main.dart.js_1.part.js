91d898d2f0e7a2113f5932115256b3e827b75383
aliyazdi75 <ma.yazdi75@gmail.com>
2021-01-24 11:35:19 +0330

    Built web
    Branch: main
    Commit: cc4513c6e3172b0d4a556d3e5249f5da60ddf4bc (dirty)
    
    package:peanut 3.5.1

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,5 +1,5 @@
 self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var C,H,J,P,W,U,Y,N={f0:function f0(){}},R,B,A,G,V,X,S,Z,E,T,K,L,O,D,F,Q,M
+$__dart_deferred_initializers__.current=function(a,b,c,$){var C,H,J,P,W,U,Y,N={f6:function f6(){}},R,B,A,G,V,X,S,Z,E,T,K,L,O,D,F,Q,M
 a.setFunctionNamesIfNecessary([N])
 C=c[0]
 H=c[1]
@@ -26,10 +26,10 @@ D=c[21]
 F=c[22]
 Q=c[23]
 M=c[24]
-N.f0.prototype={}
+N.f6.prototype={}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(N.f0,P.B)})()
-H.a7Z(b.typeUniverse,JSON.parse('{"aaC":"T","abf":"T","aaI":"fl","aaD":"t","abI":"t","abV":"t","aaB":"a4","abl":"a4","acw":"dl","aaL":"a3","abW":"X","ab8":"X","abm":"eP","ace":"dp","aaX":"f9","ab5":"ea","aaS":"dv","ac4":"dv","abE":"hr","abo":"hk","abn":"hj","ab_":"bt","aaJ":"hp"}'))
+y(N.f6,P.z)})()
+H.a9_(b.typeUniverse,JSON.parse('{"abF":"T","ach":"T","abK":"fq","abG":"u","acL":"u","acW":"u","abE":"a5","acn":"a5","adx":"dn","abN":"a2","acX":"Y","ac9":"Y","aco":"eV","adf":"ds","abY":"fd","ac6":"eg","abT":"dx","ad5":"dx","acH":"ht","acq":"hn","acp":"hm","ac0":"bv","abL":"hs"}'))
 0
 0}
-$__dart_deferred_initializers__["2ndoyJ3ltbeUstZOWlyCO5EWGQI="] = $__dart_deferred_initializers__.current
+$__dart_deferred_initializers__["D9jVXT+zGx8j0jM+NEwuGjotttA="] = $__dart_deferred_initializers__.current
--- main.dart.js_2.part.js
+++ main.dart.js_2.part.js
@@ -1,8 +1,8 @@
 self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
 $__dart_deferred_initializers__.current=function(a,b,c,$){var C,H,J,P,W,U,Y,N,R,B,A,G,V,X,S={
-ar4:function(){X.qv("en")
-return new S.lB()},
-lB:function lB(){}},Z,E,T,K,L,O,D,F,Q,M
+asa:function(){X.qx("en")
+return new S.lE()},
+lE:function lE(){}},Z,E,T,K,L,O,D,F,Q,M
 a.setFunctionNamesIfNecessary([S])
 C=c[0]
 H=c[1]
@@ -29,57 +29,57 @@ D=c[21]
 F=c[22]
 Q=c[23]
 M=c[24]
-S.lB.prototype={
-gye:function(){return"Ali Yazdi Homepage"},
-gIC:function(){return"ALI YAZDI"},
-gID:function(){return"CE GRADUATED @ TEHRAN POLYTECHNIC"},
-gK_:function(){return"Hi there, welcome to my page"},
-gFt:function(){return"I have been working as a Flutter developer and mobile team leader for two years. I am a CS graduate who interests in using Artificial Intelligence and Software Engineering in my work experiences. I am an open-source advocate who helps developers to solve their problems in development. I am finding a better opportunity and willing to relocate to a new country."},
-gJt:function(d){return"Resume"},
-gFu:function(){return"ABOUT"},
-gH0:function(){return"EDUCATION"},
-gHa:function(){return"EXPERIENCE"},
-gFW:function(){return"AWARDS"},
-gFN:function(){return"Amirkabir University of Technology"},
-gIV:function(){return"(Tehran Polytechnic)"},
-gFJ:function(){return", Tehran, Iran"},
-gFL:function(){return"B.Sc., Computer Engineering, Software"},
-gFM:function(){return"September 2015"},
-gFK:function(){return"January 2020"},
-gzK:function(){return"Adab High School"},
-gzG:function(){return"Tehran, Iran"},
-gzJ:function(){return"September 2011"},
-gzH:function(){return"June 2014"},
-gzI:function(){return"Diploma in Physics and Mathematics"},
-gJE:function(){return"Tarafdari"},
-gJB:function(){return"Tehran, Iran"},
-gJG:function(){return"March 2019"},
-gJF:function(){return"May 2020"},
-gJD:function(){return"Flutter, Drupal, Laravel, MySQL"},
-gJC:function(){return"Working as a mobile application team leader at one of the most popular sport social media."},
-gFP:function(){return"Application Page"},
-gIp:function(){return"Mate Machine Inc."},
-gIm:function(){return"Vancouver, BC, Canada"},
-gIr:function(){return"June 2019"},
-gIq:function(){return"Present"},
-gIo:function(){return"Flutter"},
-gIn:function(){return"Working as a flutter developer team leader to launch the application in three months."},
-gIH:function(){return"July 2013"},
-gII:function(){return"Qualified for the Second Round of the Iranian Physics Olympiad and Mathematical Olympiad"},
-gH8:function(){return"July 2015"},
-gH9:function(){return"Ranked in top 0.2% among more than 180K participants in Nation-Wide University Entrance Exam among all Iranian Student of Physics and Mathematics"},
-gG0:function(){return"February 2020"},
-G1:function(d){return"First Place in the 12th Iran Web and Mobile Festival("+d+") for the Best Sport Mobile Application among 7k applications"},
-gG2:function(){return"IWMF"},
-gIk:function(){return"Made with"},
-Ga:function(d){return"by "+d},
-gHo:function(){return"Flutter"},
-gIy:function(){return"February 2020"},
-Iz:function(d){return"Most installed sport application in Iran, we could reach more than 200k active users on the "+d+" after migrating to the flutter"},
-gIA:function(){return"Google Play Store"}}
+S.lE.prototype={
+gyf:function(){return"Ali Yazdi Homepage"},
+gIz:function(){return"ALI YAZDI"},
+gIA:function(){return"CE GRADUATED @ TEHRAN POLYTECHNIC"},
+gJX:function(){return"Hi there, welcome to my page"},
+gFq:function(){return"I have been working as a Flutter developer and mobile team leader for two years. A Software Engineering graduate who interests in using Artificial Intelligence and Software Engineering in my work experiences. An open-source advocate who helps developers to solve their problems in development. I am looking for a better opportunity and willing to relocate anywhere."},
+gJq:function(d){return"Resume"},
+gFr:function(){return"ABOUT"},
+gH_:function(){return"EDUCATION"},
+gH8:function(){return"EXPERIENCE"},
+gFR:function(){return"AWARDS"},
+gFI:function(){return"Amirkabir University of Technology"},
+gIS:function(){return"(Tehran Polytechnic)"},
+gFE:function(){return", Tehran, Iran"},
+gFG:function(){return"B.Sc., Computer Engineering, Software"},
+gFH:function(){return"September 2015"},
+gFF:function(){return"January 2020"},
+gzG:function(){return"Adab High School"},
+gzC:function(){return"Tehran, Iran"},
+gzF:function(){return"September 2011"},
+gzD:function(){return"June 2014"},
+gzE:function(){return"Diploma in Physics and Mathematics"},
+gJA:function(){return"Tarafdari"},
+gJx:function(){return"Tehran, Iran"},
+gJC:function(){return"March 2019"},
+gJB:function(){return"May 2020"},
+gJz:function(){return"Flutter, Drupal, Laravel, MySQL"},
+gJy:function(){return"Working as a mobile application team leader at one of the most popular sport social media."},
+gFK:function(){return"Application Page"},
+gIm:function(){return"Mate Machine Inc."},
+gIj:function(){return"Vancouver, BC, Canada"},
+gIo:function(){return"June 2019"},
+gIn:function(){return"Present"},
+gIl:function(){return"Flutter"},
+gIk:function(){return"Working as a flutter developer team leader to launch the application in three months."},
+gIE:function(){return"July 2013"},
+gIF:function(){return"Qualified for the Second Round of the Iranian Physics Olympiad and Mathematical Olympiad"},
+gH6:function(){return"July 2015"},
+gH7:function(){return"Ranked in top 0.2% among more than 180K participants in Nation-Wide University Entrance Exam among all Iranian Student of Physics and Mathematics"},
+gFX:function(){return"February 2020"},
+FY:function(d){return"First Place in the 12th Iran Web and Mobile Festival("+d+") for the Best Sport Mobile Application among 7k applications"},
+gFZ:function(){return"IWMF"},
+gIh:function(){return"Made with"},
+G7:function(d){return"by "+d},
+gHk:function(){return"Flutter"},
+gIv:function(){return"February 2020"},
+Iw:function(d){return"Most installed sport application in Iran, we could reach more than 200k active users on the "+d+" after migrating to the flutter"},
+gIx:function(){return"Google Play Store"}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(S.lB,N.f0)})()
-H.a7Z(b.typeUniverse,JSON.parse('{"aaC":"T","abf":"T","aaI":"fl","aaD":"t","abI":"t","abV":"t","aaB":"a4","abl":"a4","acw":"dl","aaL":"a3","abW":"X","ab8":"X","abm":"eP","ace":"dp","aaX":"f9","ab5":"ea","aaS":"dv","ac4":"dv","abE":"hr","abo":"hk","abn":"hj","ab_":"bt","aaJ":"hp","lB":{"f0":[]}}'))
+y(S.lE,N.f6)})()
+H.a9_(b.typeUniverse,JSON.parse('{"abF":"T","ach":"T","abK":"fq","abG":"u","acL":"u","acW":"u","abE":"a5","acn":"a5","adx":"dn","abN":"a2","acX":"Y","ac9":"Y","aco":"eV","adf":"ds","abY":"fd","ac6":"eg","abT":"dx","ad5":"dx","acH":"ht","acq":"hn","acp":"hm","ac0":"bv","abL":"hs","lE":{"f6":[]}}'))
 0
 0}
-$__dart_deferred_initializers__["3t4R5M6ky9ckI8em6PdIeF9tsOY="] = $__dart_deferred_initializers__.current
+$__dart_deferred_initializers__["2hr5NU93YYAZpApwNuhm0juPxpQ="] = $__dart_deferred_initializers__.current
